3f42f5538929f007e3c5bfffbe23bcd27a31a745
Andrés Haskel <andres.haskel@gmail.com>
2017-06-15 14:56:13 -0300

    Urls added

---
+++ socialNetwork/socialNetworkApp/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import url
+from socialNetworkApp.views import UserView, UserFriendsView, RankingView,IndexView, LoginView, LogoutView
+from django.contrib.auth.views import login
+from django.contrib.auth.views import logout
+
+
+urlpatterns = [
+    url(r'^$', IndexView.as_view(), name= "index"),
+    url(r'^user/login/$', LoginView.as_view(), name= "login"),
+    url(r'^friends/$', UserFriendsView.as_view(), name= "friends"),
+    url(r'^ranking$', RankingView.as_view(), name = "ranking"),
+    url(r'^logout/$', LogoutView.as_view()),
+    url(r'^addFriend/$', UserView.as_view(), name = "users"),
+
+]
