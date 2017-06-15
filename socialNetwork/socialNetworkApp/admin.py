5bd6999d8226e4b8c117d70e075a27598ee79efb
Andrés Haskel <andres.haskel@gmail.com>
2017-06-15 14:55:01 -0300

    Admin
    user registered in admin

--- socialNetwork/socialNetworkApp/admin.py
+++ socialNetwork/socialNetworkApp/admin.py
@@ -2,5 +2,9 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User, Post
 
 # Register your models here.
+admin.site.register(User, UserAdmin)
+admin.site.register(Post)
