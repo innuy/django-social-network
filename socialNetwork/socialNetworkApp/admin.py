50dd2533d9648bd68375fd7612e4ee617a6c85cb
Andrés Haskel <andres.haskel@gmail.com>
2017-06-16 16:21:18 -0300

    Admin
    Register models on the admin

--- socialNetwork/socialNetworkApp/admin.py
+++ socialNetwork/socialNetworkApp/admin.py
@@ -3,8 +3,9 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User, Post
+from .models import User, Post, UserFriend
 
 # Register your models here.
 admin.site.register(User, UserAdmin)
 admin.site.register(Post)
+admin.site.register(UserFriend)
