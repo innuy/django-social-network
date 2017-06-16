f4037fa9221a596ee958e222aaf56119a41a321e
Andrés Haskel <andres.haskel@gmail.com>
2017-06-16 16:24:47 -0300

    URLS
    name "logout" added to url

--- socialNetwork/socialNetworkApp/urls.py
+++ socialNetwork/socialNetworkApp/urls.py
@@ -8,8 +8,7 @@ urlpatterns = [
     url(r'^$', IndexView.as_view(), name= "index"),
     url(r'^user/login/$', LoginView.as_view(), name= "login"),
     url(r'^friends/$', UserFriendsView.as_view(), name= "friends"),
-    url(r'^ranking$', RankingView.as_view(), name = "ranking"),
-    url(r'^logout/$', LogoutView.as_view()),
-    url(r'^addFriend/$', UserView.as_view(), name = "users"),
-
+    url(r'^ranking/$', RankingView.as_view(), name = "ranking"),
+    url(r'^logout/$', LogoutView.as_view(), name="logout"),
+    url(r'^users/$', UserView.as_view(), name = "users"),
 ]
