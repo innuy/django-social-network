7cca35e2be8d6da2a5435cbc73bca64234e69a35
Andrés Haskel <andres.haskel@gmail.com>
2017-06-16 16:26:50 -0300

    Documentation of views
    Documentation of models explaining functionalities

--- socialNetwork/socialNetworkApp/views.py
+++ socialNetwork/socialNetworkApp/views.py
@@ -6,6 +6,7 @@ from _curses import use_default_colors
 from django.contrib import auth
 from django.core.exceptions import ObjectDoesNotExist
 from django.urls import reverse
+from rest_framework import permissions
 
 from serializers import UserSerializer
 
@@ -24,107 +25,98 @@ from models import User, UserFriend, Post
 # class ValidateUserLogged(APIView):
 #    def get(self, request):
 
+class RestrictedView(APIView):
+    permission_classes = (permissions.IsAuthenticated,)
 
-class UserView(APIView):
+
+class UserView(RestrictedView):
     def get(self, request):
-        try:
-            user = User.objects.get(id=request.user.id)
+        """
+        Get all the users that are not on the user friends list
+        :return: render
+        """
+        users_dictionary = list(request.user.get_unknown_users().values('id', 'username'))
+        logged_user = {'logged_user': request.user}
 
-            if user.is_authenticated:
-                users = User.objects.all()
-                logged_user = {'logged_user': request.user}
-                users_dictionary = []
-                for user in users:
-                    dictionary = {
-                        'id': user.id,
-                        'username': user.username,
-                    }
-                    users_dictionary.append(dictionary)
-
-                return render(request, 'addFriend.html', {'users': users_dictionary, 'logged_user': logged_user})
-            else:
-                return HttpResponse(status=401)
-
-        except User.DoesNotExist:
-            return HttpResponse(status=403)
+        return render(request, 'addFriend.html', {'users': users_dictionary, 'logged_user': logged_user})
 
 
-class UserFriendsView(APIView):
+class UserFriendsView(RestrictedView):
     def get(self, request):
-        try:
-            user = User.objects.get(id=request.user.id)
+        """
+        Get all the user friends
+        :return: renders userFriends.html or status code 403 if user is not logged in
+        """
+        user = request.user
+        user_friend_dictionary = []
+        users = UserFriend.get_user_friends(user.id)
+        for user in users:
+            dictionary = {
+                'id': user.id,
+                'username': user.username,
+            }
+            user_friend_dictionary.append(dictionary)
 
-            if user.is_authenticated:
-                user = request.user
-                user_friend_dictionary = []
-                friends = UserFriend.get_user_friends(user.id)
-                for friend in friends:
-                    dictionary = {
-                        'id': friend.id,
-                        'first_user': friend.first_user.username,
-                        'second_user': friend.second_user.username,
-                    }
-                    user_friend_dictionary.append(dictionary)
-
-                return render(request, 'userFriends.html', {'friends': user_friend_dictionary})
-            else:
-                return HttpResponse(status=401)
-
-        except User.DoesNotExist:
-            return HttpResponse(status=403)
+        return render(request, 'userFriends.html', {'users': user_friend_dictionary})
 
     def post(self, request):
+        """
+        Add a new friend
+        :return: status code 200, 403 if user is not logged in or 204 if the friend to add is not found
+        """
         try:
-            user_friend_form = request.POST
             second_user_pk = request.POST.get("second_user", None)
             second_user = User.objects.get(id=second_user_pk)
             first_user = request.user
-            if first_user.is_authenticated:
-                UserFriend.objects.create(first_user=first_user, second_user=second_user)
 
-                return HttpResponse(status=200)
-
-            else:
-                return HttpResponse(status=401)
+            UserFriend.objects.create(first_user=first_user, second_user=second_user)
+            return HttpResponse(status=200)
 
         except User.DoesNotExist:
-            return HttpResponse(status=403)
+            return HttpResponse(status=204)
 
 
 class RankingView(APIView):
+    """
+    Shows the users that spent more time online
+    :return: Json with the ranking of users
+    """
+
     def get(self, request):
-        user_ranking_dictonary = []
+        user_ranking_dictionary = []
         top_ten_users = User.top_ten_logged_users()
 
         for user in top_ten_users:
             dictonary = {
-                'user': UserSerializer(user)
+                'username': user.username,
+                'time_online': user.time_spent_online,
             }
-            user_ranking_dictonary.append(dictonary)
+            user_ranking_dictionary.append(dictonary)
 
-        return JsonResponse(user_ranking_dictonary, safe=False)
+        return JsonResponse({'users': user_ranking_dictionary}, safe=False)
 
 
-class IndexView(APIView):
+class IndexView(RestrictedView):
     def get(self, request):
-        posts_dictionary = []
+        """
+        Get all user messages and the logged in user
+        :return: render index.html on success or login.html if user isn't authorized
+        """
+
         logged_user = {'logged_user': request.user}
-        posts = Post.objects.all().order_by("date_posted")
-        for post in posts:
-            dictionary = {
-                'title': post.title,
-                'description': post.description,
-                'date_posted': post.date_posted,
-                'author': post.author
-            }
-            posts_dictionary.append(dictionary)
+        posts_dictionary = list(
+        Post.objects.all().order_by("-date_posted").values('id', 'title', 'description', 'date_posted',
+                                                               'author'))
 
         return render(request, 'index.html', {'posts': posts_dictionary, 'user': logged_user})
 
 
 class LoginView(APIView):
     def post(self, request):
-
+        """
+        User authentication
+        :return: redirects to index on success or reloads login.html on failure
+        """
         user = auth.authenticate(request, username=request.POST['username'], password=request.POST['password'])
         if user is not None:
             auth.login(request, user)
@@ -134,11 +126,13 @@ class LoginView(APIView):
             return render(request, 'registration/login.html', status=403)
 
 
-class LogoutView(APIView):
+class LogoutView(RestrictedView):
     def post(self, request):
+        """
+        User logout
+        :return: Status code 200 on success or 403 if user isn't logged in
+        """
         user = request.user
-        if user.is_authenticated:
-            auth.logout(request)
-            return HttpResponse(status=200)
-
-        return HttpResponse(status=403)
+        user.set_time_online()
+        auth.logout(request)
+        return HttpResponse(status=200)
