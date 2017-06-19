8d2afb951588e80e1f694b48b10f344fbf95e4b6
Andrés Haskel <andres.haskel@gmail.com>
2017-06-19 16:04:57 -0300

    Change in model
    Override method save in UserFriend model

--- socialNetwork/socialNetworkApp/models.py
+++ socialNetwork/socialNetworkApp/models.py
@@ -1,16 +1,16 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import logging
+
+from django.contrib.auth.models import AbstractUser
+from django.db import models
 from django.db.models import Q
 from django.utils import timezone
 
-from MySQLdb.constants.FLAG import UNIQUE
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-# Create your models here.
 from socialNetwork import settings
-import logging
+
+logger = logging.getLogger(__name__)
 
 
 class UserFriend(models.Model):
@@ -21,25 +21,29 @@ class UserFriend(models.Model):
     def get_user_friends(user_id):
         """
         Get user friends from a user id
-        :param user_id: User identifier (integer)
-
+        :type user_id: int
+        :param user_id: User identifier
         :return: list of user objects
         """
-        friendships = UserFriend.objects.filter(Q(first_user=user_id) | Q(second_user=user_id))
-        user_friends = []
-        for friendship in friendships:
-            try:
-                # Select friend from UserFriend relationship
-                if friendship.first_user.id != user_id:
-                    user_friends.append(friendship.first_user)
-                else:
-                    user_friends.append(friendship.second_user)
-            except Exception as e:
-                # [TODO] see log
-                # TODO: check log traceback
-                logger.exception(e)
+        friendship_list = UserFriend.objects.filter(Q(first_user=user_id) | Q(second_user=user_id))
+
+        user_friends = [friendship.first_user if friendship.first_user.id != user_id else friendship.second_user for
+                        friendship in friendship_list.iterator()]
 
         return user_friends
+
+    def save(self, *args, **kwargs):
+        """
+        Always saves the user with lesser id on the first
+        user and the greater on the second user.
+        Doing this we avoid to add repeated friendships
+        """
+
+        greater = self.first_user if self.first_user.id >= self.second_user.id else self.second_user
+        lesser = self.first_user if greater == self.second_user else self.second_user
+        self.first_user = lesser
+        self.second_user = greater
+        super(UserFriend, self).save(*args, **kwargs)
 
     class Meta:
         unique_together = ("first_user", "second_user")
@@ -47,7 +51,7 @@ class UserFriend(models.Model):
 
 class User(AbstractUser):
     time_spent_online = models.IntegerField(default=0)
-    friends = models.ManyToManyField(UserFriend)
+    friends = models.ManyToManyField(UserFriend, through='UserFriend')
 
     @staticmethod
     def top_ten_logged_users():
@@ -55,12 +59,8 @@ class User(AbstractUser):
         Get the 10 users that spent more time online
         :return: list of user objects
         """
-        top_ten_users = []
         users = User.objects.order_by("time_spent_online")[:10]
-        for user in users:
-            top_ten_users.append(user)
-
-        return top_ten_users
+        return [user for user in users.iterator()]
 
     def get_unknown_users(self):
         """
@@ -70,7 +70,6 @@ class User(AbstractUser):
         users = UserFriend.get_user_friends(self.id)
         friends_ids = [user.pk for user in users]
         unknown_users = User.objects.exclude(pk__in=friends_ids + [self.id])
-
         return unknown_users
 
     def set_time_online(self):
@@ -78,10 +77,10 @@ class User(AbstractUser):
         Update the time that the user spent logged in.
         Triggered by logout operation.
         """
-        user = User.objects.get(id=self.id)
+        user = self
         time_logged = timezone.now() - user.last_login
-        self.time_spent_online += time_logged.total_seconds() // 60
-        User.save(self)
+        user.time_spent_online += time_logged.total_seconds() // 60
+        user.save()
 
     class Meta:
         db_table = 'auth_user'
@@ -91,7 +90,7 @@ class Post(models.Model):
     """
     User messages
     """
-    title = models.CharField(max_length=10)
+    title = models.CharField(max_length=10, null=False)
     description = models.CharField(max_length=50)
     date_posted = models.DateTimeField(blank=True, default=timezone.now)
     author = models.ForeignKey('User', on_delete=models.CASCADE)
